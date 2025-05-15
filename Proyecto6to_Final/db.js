8224ece2cf4a125caa91f8a572ac6bc65f6e29e5
ProgramadorMaest <160616553+ProgramadorMaest@users.noreply.github.com>
2025-05-15 11:24:20 -0400

    Update db.js

--- Proyecto6to_Final/db.js
+++ Proyecto6to_Final/db.js
@@ -1,15 +1,1 @@
-const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  database: 'reservas_turisticas'
-});
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Conectado a MySQL');
-});
-
-module.exports = connection;
